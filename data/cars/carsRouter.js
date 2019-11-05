236cc88d979987eb66bb1f0761844a037b36d262
seanaleid <seanaleid@gmail.com>
2019-11-05 17:35:59 -0500

    Adds function GET /:id using knex

--- data/cars/carsRouter.js
+++ data/cars/carsRouter.js
@@ -6,7 +6,10 @@ const knex = require('../../dbConfig.js');
 
 const router = express.Router();
 
-// basic GET / using knex dbConfig.js
+// refactored code using helpers
+// router.get('/', )
+
+// original using knex basic GET / using knex dbConfig.js
 router.get('/', (req, res) => {
     
     knex 
@@ -20,6 +23,36 @@ router.get('/', (req, res) => {
             res.status(500).json({ error: 'Failed to get cars from database'})
         })
 
+})
+
+// original using knex basic POST / using knex dbConfig.js
+router.post('/', (req, res) => {
+
+    knex   
+        .insert(req.body)
+        .into('cars')
+        .then(id => {
+            res.status(201).json(id);
+        })
+        .catch(err => {
+            res.status(500).json({ error: `Failed to add the car to the database`});
+        });
+});
+
+// original using knex basic GET /:id using knex dbConfig.js
+router.get('/:id', (req, res) => {
+
+    knex
+        .select('*')
+        .from('cars')
+        .where('id', '=', req.params.id)
+        .first()
+        .then(car => {
+            res.status(200).json(car);
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'Failed to get the car from the database.'})
+        })
 })
 
 module.exports = router;
